6c345323ec97a80d0d645c06dc7e290fe005d7ed
Paras Jain <paras.alberteinstien@gmail.com>
2020-04-16 17:00:08 +0530

    version-0.0.01

---
+++ hello.java
@@ -0,0 +1,15 @@
+class hello
+{
+    int check(int num, int d){
+
+
+
+    }
+
+    public static void main(String[] args)
+    {
+        hello palindrome = new hello();
+        int arr[] = {3,7,-1,2,-1,5,-1,-1,-1,1,0};
+        
+    }
+}
